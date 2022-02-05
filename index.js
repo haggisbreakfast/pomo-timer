2acfb86992b48da8a4f90713e4d2711e090c4df8
Lindsay Stewart <lj.stewart00@gmail.com>
2022-02-05 00:42:29 -0800

    add state, handleStart & decreaseTimer methods

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 new Vue({
 	el: '#app', 
-	data: {}, 
-	methods: {}, 
+	data: {
+		remainingTime: 60 * 25,
+		timer: null,
+		name: 'homo pomo'
+	}, 
+	methods: {
+		handleStart: function() {
+			this.timer = setInterval(() => this.decreaseTimer(), 1000)
+		},
+		decreaseTimer: function() {
+			if (this.remainingTime >= 1) {
+				this.remainingTime --;
+			} else {
+				this.remainingTime = 0;
+				this.handlePause();
+				alert("yas time's up go off kween werk")
+			}
+		}
+		
+	}, 
 	computed: {}
 })
